86de4967807d975c5cb630c9305b078b503d2e11
choznerol <lawrencechou1024@gmail.com>
2018-05-21 00:57:18 +0800

    ✨ 初始化資料庫 company.Client_info 的 script

---
+++ scripts/init_database.py
@@ -0,0 +1,37 @@
+import os
+import pymysql
+
+"""
+連結本地的 mysql 資料庫，並創建 `company` database，包含一個 `user` table
+"""
+
+config = {
+  'host': input('> 輸入 mysql 所在 host (預設 "127.0.0.1")：') or '127.0.0.1',
+  'user': input('> 輸入 mysql 使用者名稱 (預設 "pi")：') or 'pi',
+  'password': input(f'> 輸入使用者的密碼：'),
+}
+
+conn = pymysql.connect(**config)
+cursor = conn.cursor()
+
+tasks = [
+  "DROP DATABASE IF EXISTS company;",
+  "CREATE DATABASE company;",
+  "USE company;",
+  "CREATE TABLE Client_info (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), color INT(255), send_status INT(255));",
+  "INSERT INTO Client_info(name, color, send_status) VALUES('Angela', 0, 0), ('Warren', 0, 0), ('Tobby', 0, 0), ('Tom', 0, 0), ('Johnson', 0, 0);"
+]
+
+for index, sql in enumerate(tasks):
+  print(f'[{index+1}/{len(tasks)}] 執行: {sql}')
+  try:
+    cursor.execute(sql)
+    conn.commit()
+  except:
+    conn.rollback()
+    conn.close()
+    print(f'SQL執行失敗：{sql}')
+    print('資料庫已回滾，即將退出...')
+    exit(1)
+
+conn.close()
