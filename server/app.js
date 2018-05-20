cd35ab86b46b87a401e898a6338160f64fac7b03
choznerol <lawrencechou1024@gmail.com>
2018-05-21 00:56:34 +0800

    ✨ 連接 MariaDB
    參考教學：https://dotblogs.com.tw/explooosion/2016/07/18/010601
    相關文黨：https://expressjs.com/zh-tw/guide/database-integration.html#mysql

--- server/app.js
+++ server/app.js
@@ -1,14 +1,31 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-
+const mysql = require('mysql');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOST;
+const database = process.env.MYSQL_DATABASE;
+
+// 連結資料庫
+const db = mysql.createConnection({ host, user, password, database });
+db.connect(err => {
+  const info = `使用者：${user}，密碼：${password ? password.replace(/./g, "*") : "未輸入（請檢查 .env）"}`;
+  if (err) {
+    console.error('連接資料庫失敗' + info);
+    throw err;
+  }
+  console.log('成功連接資料庫' + info);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +42,12 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 提供 db instance
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
