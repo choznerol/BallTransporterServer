a451e2b2a19b83a18e0131ccd16180a34d9f7093
choznerol <lawrencechou1024@gmail.com>
2018-05-21 00:58:59 +0800

    🔧儲存 log 到 access.log（使用 morgan）

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 const mysql = require('mysql');
+var fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -31,7 +32,9 @@ db.connect(err => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(logger("dev", { stream: accessLogStream }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
