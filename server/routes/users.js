22e9b655bec75aee1b997f4771a03f18208739f9
choznerol <lawrencechou1024@gmail.com>
2018-06-01 15:26:16 +0800

    ✨Add API in /user: create_order, send_status, cancel_order

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,4 +14,56 @@ router.get('/', function(req, res, next) {
   });
 });
 
+
+/**
+ * Create new order for the user
+ *
+ * example:
+ *   GET 192.168.X.X/users/2/create_order?merchandise=1&destination=2
+ */
+router.get(/\d+\/create_order/, function (req, res, next) {
+  const { merchandise, destination } = req.query;
+
+  if (!merchandise || !destination) {
+    res.status(400).send('未提供 Query string `merchandise` 及 `destination`');
+    console.error('未提供 Query string `merchandise` 及 `destination`');
+  }
+
+  const clientId = req.url.split('/')[1]
+  res.send(`TODO: 嘗試為 User#${clientId} 建立訂單`)
+})
+
+
+/**
+ * Get the sending status of order for the user
+ *
+ * example:
+ *   GET 192.168.X.X/users/2/send_status
+ */
+router.get(/\d+\/send_status/, function (req, res, next) {
+  const clientId = req.url.split('/')[1]
+  const sql = `SELECT send_status FROM Client_info WHERE id = ${clientId}`
+
+  req.db.query(sql, function(err, rows, fields) {
+    if (err) throw err;
+    if (rows[0]) {
+      res.send(rows[0]);
+    } else {
+      res.status(404).end();
+    }
+  });
+})
+
+
+/**
+ * Cancel order for a client
+ *
+ * example:
+ *   GET 192.168.X.X/users/2/cancel_order
+ */
+router.get(/\d+\/cancel_order/, function(req, res, next) {
+  const clientId = req.url.split('/')[1]
+  res.send(`TODO: 嘗試為 User#${clientId} 取消訂單`)
+})
+
 module.exports = router;
