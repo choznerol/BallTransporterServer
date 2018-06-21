73bcbc13e7c6faa91d19e4f8828b978f6ee78fe9
choznerol <lawrencechou1024@gmail.com>
2018-06-22 00:58:39 +0800

    add test

---
+++ modules/opencv-color-ball-transporter-cpp/test.js
@@ -0,0 +1,4 @@
+const path = require("path");
+const run = require("./run.js");
+
+run(`${path.resolve(__dirname, "./")}/test.sh`);
---
+++ modules/opencv-color-ball-transporter-cpp/test.sh
@@ -0,0 +1,8 @@
+echo 1
+sleep 1s
+echo 2
+sleep 1s
+echo 3
+sleep 1s
+exit 0
+# exit 1  # error will be logged
